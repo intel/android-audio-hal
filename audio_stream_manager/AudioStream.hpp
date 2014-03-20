d127639d012ec4523f5aa93f0e4f4a09fcf96563
ptonnetx <pierre-louisx.tonnet@intel.com>
2014-03-20 18:26:02 +0100

    [AudioComms] Reduce Lock range to necessary
    BZ: 176558
    
    There is an ANR that occurs during monkey test and MTBF:
    Record thread calls setApplicabilityMask which autolocks in write mode
    _streamlock. In this lock context, is called updateLatency, which calls
    AudioRouteManager:getLatencyInUs who waits _routingLock to be unlocked.
    _routingLock is currently locked in write mode by Playback thread in
    reconsiderRouting. which calls AudioStream::isStarted who waits _streamLock
    to be unlocked. So there is a deadlock.
    
    To avoid that issue, we remove the Autolock in write mode of _streamlock in
    setApplicabilityMask, and only lock it when writing. We add also a lock in read
    mode in getApplicabilityMask.
    
    Change-Id: I53382be838a9783080025d6b09950e10c52673cf
    Signed-off-by: ptonnetx <pierre-louisx.tonnet@intel.com>

--- audio_stream_manager/AudioStream.hpp
+++ audio_stream_manager/AudioStream.hpp
@@ -1,6 +1,6 @@
 /*
  * INTEL CONFIDENTIAL
- * Copyright © 2013 Intel
+ * Copyright (c) 2013-2014 Intel
  * Corporation All Rights Reserved.
  *
  * The source code contained or described herein and all documents related to
@@ -11,7 +11,7 @@
  * Material is protected by worldwide copyright and trade secret laws and
  * treaty provisions. No part of the Material may be used, copied, reproduced,
  * modified, published, uploaded, posted, transmitted, distributed, or
- * disclosed in any way without Intel’s prior express written permission.
+ * disclosed in any way without Intel's prior express written permission.
  *
  * No license under any patent, copyright, trade secret or other intellectual
  * property right is granted to or conferred upon you by disclosure or delivery
@@ -172,9 +172,16 @@ public:
      */
     virtual uint32_t getApplicabilityMask() const
     {
+        AutoR lock(_streamLock);
         return _applicabilityMask;
     }
 
+    /**
+     * Set the Applicability mask.
+     * This function is non-reetrant.
+     *
+     * @param[in] applicabilityMask: ID of input source if input, stream flags if output.
+     */
     void setApplicabilityMask(uint32_t applicabilityMask);
 
     /**
