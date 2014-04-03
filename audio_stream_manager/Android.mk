9ceb149b88cbc380cda5ec53c86df2c6dfb73750
Francois Gaffie <francois.gaffie@intel.com>
2014-04-03 11:59:52 +0200

    Clean-up audioHAL makefile
    BZ: 183846
    
    Android.mk contains dead sections.
    
    This patch cleans the makefile.
    
    Change-Id: Id87fc1dd4ceaf4398162c98db23c400075957888
    Signed-off-by: Francois Gaffie <francois.gaffie@intel.com>

--- audio_stream_manager/Android.mk
+++ audio_stream_manager/Android.mk
@@ -1,6 +1,6 @@
 #
 # INTEL CONFIDENTIAL
-# Copyright © 2013 Intel
+# Copyright (c) 2013-2014 Intel
 # Corporation All Rights Reserved.
 #
 # The source code contained or described herein and all documents related to
@@ -11,7 +11,7 @@
 # Material is protected by worldwide copyright and trade secret laws and
 # treaty provisions. No part of the Material may be used, copied, reproduced,
 # modified, published, uploaded, posted, transmitted, distributed, or
-# disclosed in any way without Intel’s prior express written permission.
+# disclosed in any way without Intel's prior express written permission.
 #
 # No license under any patent, copyright, trade secret or other intellectual
 # property right is granted to or conferred upon you by disclosure or delivery
@@ -55,19 +55,6 @@ audio_stream_manager_includes_dir_target := \
     $(audio_stream_manager_includes_dir) \
     $(call include-path-for, stlport) \
     $(call include-path-for, bionic)
-
-audio_stream_manager_header_files :=  \
-    AudioStream.h \
-    AudioDumpInterface.h \
-    AudioIntelHAL.h \
-    AudioParameterHandler.h \
-    AudioPlatformHardware.h \
-    AudioPlatformState.h \
-    AudioStreamIn.h \
-    AudioStreamOut.h
-
-audio_stream_manager_header_copy_folder_unit_test := \
-    audio_stream_manager_unit_test
 
 audio_stream_manager_static_lib += \
     libsamplespec_static \
@@ -187,8 +174,6 @@ include $(BUILD_SHARED_LIBRARY)
 # Compile macro
 define make_audio_stream_manager_test_lib
 $( \
-    $(eval LOCAL_COPY_HEADERS_TO := $(audio_stream_manager_header_copy_folder_unit_test)) \
-    $(eval LOCAL_COPY_HEADERS := $(audio_stream_manager_header_files)) \
     $(eval LOCAL_C_INCLUDES := $(audio_stream_manager_includes_dir_$(1))) \
     $(eval LOCAL_STATIC_LIBRARIES := $(audio_stream_manager_static_lib_$(1))) \
     $(eval LOCAL_WHOLE_STATIC_LIBRARIES := $(audio_stream_manager_whole_static_lib)) \
